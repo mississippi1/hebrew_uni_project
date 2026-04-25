d38b97d36078eef5bdda27c1e2d6c3fe2ce6daab
Tomer Peker <tomer@copilot.cx>
2026-04-25 12:50:02 +0300

    commit

--- oop/ex1/SmartPlayer.java
+++ oop/ex1/SmartPlayer.java
@@ -19,6 +19,19 @@ public class SmartPlayer implements Player {
     @Override
     public void playTurn(Board board, Mark mark) {
 		boolean success;
+		int[] moveWithLongestStreakIfExists = moveWithLongestMarks(board, mark);
+		if(moveWithLongestStreakIfExists !=null){
+			success = board.putMark(mark, moveWithLongestStreakIfExists[0], moveWithLongestStreakIfExists[1]);
+			if (success) {
+				return;
+			}
+		}
+		for(int row = 0; row < board.getSize(); row++) {
+			success = board.putMark(mark, row, 1);
+			if (success) {
+				return;
+			}
+		}
 		for (int index = 0; index < board.getSize(); index++) {
 			success = board.putMark(mark, index, index+1);
 			if (success) {
@@ -46,4 +59,44 @@ public class SmartPlayer implements Player {
         } while (!success);
     }
 
+	private int[] moveWithLongestMarks(Board board, Mark targetMark) {
+		int bestStreak = 0;
+		int[] bestMove = null;
+		for (int row = 0; row < board.getSize(); row++) {
+			for (int col = 0; col < board.getSize(); col++) {
+				if (board.getMark(row, col) != Mark.BLANK) continue;
+				int streak = countMarksInARowOrDiag(board, row, col, targetMark);
+				if (streak > bestStreak) {
+					bestStreak = streak;
+					bestMove = new int[]{row, col};
+				}
+			}
+		}
+		return bestMove;
+	}
+	private int countMarksInARowOrDiag(Board board, int row, int col, Mark mark) {
+		int max = 0;
+		int[][] combinations = {{0,1}, {0,1}, {1,1}, {1,0}, {1,-1}};
+		for(int[] option : combinations){
+			int countStrekInTwoWay = countMarks(row, col, option[0], option[1], board.getSize(), mark, board)
+					+ countMarks(row, col, -1*option[0], -1*option[1], board.getSize(), mark, board);
+			if(countStrekInTwoWay > max){
+				max = countStrekInTwoWay;
+			}
+		}
+		return max;
+	}
+
+	private int countMarks(int startRow, int startCol, int movementInRow, int movmentInCol, int steps, Mark mark, Board board) {
+		int maxCount = 0;
+		for (int index = 1; index < steps; index++) {
+			Mark currentMark = board.getMark(startRow + movementInRow * index,
+					startCol + movmentInCol * index);
+			if(mark != currentMark) {return maxCount;}
+			else {
+				maxCount++;
+			}
+		}
+		return maxCount;
+	}
 }
