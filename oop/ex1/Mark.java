765b0f815b00dfdda81326a8725c6d7adc04a0e9
Tomer Peker <tomer@copilot.cx>
2026-04-30 14:47:50 +0300

    commit

--- oop/ex1/Mark.java
+++ oop/ex1/Mark.java
@@ -2,24 +2,27 @@
  * The options of roles in Tik Tak Toe
  */
 public enum Mark {
-    /** Represents an empty cell */
-    BLANK,
-    /** Represents the X player */
-    X,
-    /** represents the O player mark */
-    O;
+	/** Represents an empty cell. */
+	BLANK,
+	/** Represents the X player's mark. */
+	X,
+	/** Represents the O player's mark. */
+	O;
+
+	private static final String X_STRING = "X";
+	private static final String O_STRING = "O";
+	private static final String BLANK_STRING = " ";
 
 	/**
 	 * the string formatting of marks
 	 * @return the marks as strings
 	 */
-    @Override
-    public String toString(){
-        return switch (this) {
-            case BLANK -> null;
-            case X -> "X";
-            case O -> "O";
-        };
-    }
-};
-
+	@Override
+	public String toString() {
+		return switch (this) {
+			case BLANK -> BLANK_STRING;
+			case X -> X_STRING;
+			case O -> O_STRING;
+		};
+	}
+}
